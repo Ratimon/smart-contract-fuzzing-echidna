ea9dadbc518eca5f527f9d9367de5febe5a8a2b7
Ratimon <ratixoxo@gmail.com>
2023-03-13 08:51:19 +0700

    Chore (./src): Cleaning comment

--- src/echidna/FuzzTokenWhale.sol
+++ src/echidna/FuzzTokenWhale.sol
@@ -6,33 +6,12 @@ import {TokenWhale} from "../TokenWhale.sol";
 contract EchidnaFuzzTokenWhale is TokenWhale {
 
     address echidna_caller = msg.sender;
-    // TokenWhale tokenWhale;
 
     constructor() TokenWhale(echidna_caller)  {
-        // tokenWhale = new TokenWhale(echidna_caller);
     }
 
     function echidna_test_balance() public view returns (bool) {
         return !isComplete();
     }
 
-
-
 }
-
-// contract EchidnaFuzzTokenWhale  {
-
-//     address echidna_caller = msg.sender;
-//     TokenWhale tokenWhale;
-
-//     constructor()  {
-//         tokenWhale = new TokenWhale(echidna_caller);
-//     }
-
-//     function echidna_test_balance() public view returns (bool) {
-//         return !tokenWhale.isComplete();
-//     }
-
-
-
-// }
